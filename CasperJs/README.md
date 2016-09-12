5421027966bbd56a034935f648f40105abd304b9
bookair17 <kyoji-exterior@goo.jp>
2016-09-12 22:06:01 +0900

    ignore

---
+++ CasperJs/README.md
@@ -0,0 +1,32 @@
+# SampleCasperJs.js
+* Contributors:
+* Donate link:
+* Tags:
+* Requires at least:
+* Tested up to:
+* Stable tag:
+
+This will be bootstrap for casperjs.
+
+## Description
+`` casperjs --web-security=false --ignore-ssl-errors=true --ssl-protocol=any --cookies-file=cookies SampleCasperJs.js 2>&1 | tee log.html ``
+`` export PHANTOMJS_EXECUTABLE=/usr/local/bin/phantomjs casperjs --web-security=false --ignore-ssl-errors=true --ssl-protocol=any --cookies-file=cookies SampleCasperJs.js 2>&1 | tee log.html ``
+
+## Installation
+
+
+## ScreenShots
+
+
+## Changelog
+
+**Version 1.0**
+
+* Initial Release
+    * make referrer
+    * some page login
+    * some link click
+
+## Upgrade Notice
+
+## References
---
+++ CasperJs/SampleCasperJs.js
@@ -0,0 +1,105 @@
+'use strict';
+
+/**
+ * SampleCasperJs.js
+ * @param {string} arg1
+ * @param {string} arg2
+ * casperjs --web-security=false --ignore-ssl-errors=true --ssl-protocol=any --cookies-file=cookies SampleCasperJs.js 2>&1 | tee log.html
+ **/
+
+var casper = require('casper').create({
+	loadImages: false,
+	verbose: true,
+	logLevel: 'debug',
+	stepTimeout: 30000,
+	onError: function (self, message) {
+		console.log('FATAL: Step' + message);
+		this.exit();
+	},
+	onStepTimeout: function (self, message) {
+		console.log('TIMEOUT: Step' + message);
+		this.exit();
+	},
+	onDie: function (message, status) {
+		console.log('DIE: Step' + message + " // " + status);
+		this.exit();
+	}
+});
+
+
+casper.on('resource.requested', function (requestData, request) {
+	var deny = [
+		'ad'
+	];
+	var allow = [
+		'.js',
+		'.css'
+	];
+	var no_skip_flg = false;
+	allow.forEach(function (needle) {
+		if (requestData.url.indexOf(needle) > 0) {
+			no_skip_flg = true;
+		}
+	});
+	deny.forEach(function (needle) {
+		if (requestData.url.indexOf(needle) > 0) {
+			no_skip_flg = false;
+		}
+	});
+	if (!no_skip_flg) {
+		casper.log('Skipped: ' + requestData.url, 'info');
+		request.abort();
+	}
+});
+
+phantom.cookiesEnabled = true;
+
+var ua = "Mozilla/5.0 (X11; Linux i686; rv:45.0) Gecko/20100101 Firefox/45.0";
+casper.userAgent(ua);
+
+var fs = require('fs');
+var arg1 = casper.cli.get(1);
+var arg2 = casper.cli.get(2);
+
+casper.start('http://example.com', function () {
+	this.echo('start');
+});
+
+casper.then(function () {
+	this.echo(' >> make referrer');
+	this.evaluate(function () {
+		var url = 'http://a.example.com';
+		var link = document.createElement('a');
+		link.setAttribute('href', url);
+		link.setAttribute('id', "myTargetUrl");
+		document.body.appendChild(link);
+	});
+	this.click('a#myTargetUrl');
+});
+
+casper.then(function () {
+	if (this.getTitle() !== "login") {
+		this.echo(this.getHTML());
+		this.capture('login.png', {
+			top: 0,
+			left: 0,
+			width: 900,
+			height: 600
+		});
+		this.die(" >> Fail.", 1);
+	}
+	this.fill('form#login_form', {
+		'login': arg1,
+		'passwd': arg2
+	}, true);
+});
+
+casper.waitForSelector("h1.targetClass", function () {
+	this.echo(' >> open target');
+	this.clickLabel('click me!', 'a');
+});
+
+casper.run(function () {
+	this.echo(' >> complete');
+	this.exit();
+});
